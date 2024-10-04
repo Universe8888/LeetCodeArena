6bfb110136713f11f9a3f22e3720d56b1c88057e
Boris Manzov <borismanzov@outlook.com>
2024-10-04 15:24:15 +0300

    Create solve.py

---
+++ PythonSolutions/Easy/1. Two Sum/solve.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_to_index = {}
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            
+            if complement in num_to_index:
+                return [num_to_index[complement], i]
+            
+            num_to_index[num] = i
+        
+        return []
+
+solution = Solution()
+
+nums1 = [2, 7, 11, 15]
+target1 = 9
+print(solution.twoSum(nums1, target1))  # Output: [0, 1]
+
+nums2 = [3, 2, 4]
+target2 = 6
+print(solution.twoSum(nums2, target2))  # Output: [1, 2]
+
+nums3 = [3, 3]
+target3 = 6
+print(solution.twoSum(nums3, target3))  # Output: [0, 1]
